51e14594bc4f35693ddf2c578f8c775c1e4983fe
saberhosneydev <ssaber277@gmail.com>
2020-02-02 18:04:15 +0200

    Adding required tag

--- js/client.js
+++ js/client.js
@@ -1,22 +1,25 @@
 document.querySelector("#contactForm").addEventListener("submit", function(event) {
-   event.preventDefault();
-   let resultMessage = document.getElementById('labelData');
-   emailjs.init("user_VV0kGVLqBYwD9xWC73hfK");
-   var template_params = {
-      "Name": document.getElementById('name').value,
-      "Email": document.getElementById('email').value,
-      "Content": document.getElementById('content').value
-  }
+	event.preventDefault();
+	let resultMessage = document.getElementById('labelData');
+	let name =document.getElementById('name').value;
+	let email = document.getElementById('email').value;
+	let content =document.getElementById('content').value;
+	emailjs.init("user_VV0kGVLqBYwD9xWC73hfK");
+	var template_params = {
+		"Name": name,
+		"Email": email,
+		"Content": content
+	}
 
-  var service_id = "default_service";
-  var template_id = "website_default_form";
-  emailjs.send(service_id, template_id, template_params).then(function(response) {
-      resultMessage.classList.add('success');
-      resultMessage.style.display = "inline";
-      resultMessage.innerHTML = 'Message sent, I will reach you asap!';
-  }, function(error) {
-      resultMessage.classList.add('danger');
-      resultMessage.style.display = "inline";
-      resultMessage.innerHTML = error;
-  });;
+	var service_id = "default_service";
+	var template_id = "website_default_form";
+	emailjs.send(service_id, template_id, template_params).then(function(response) {
+		resultMessage.classList.add('success');
+		resultMessage.style.display = "inline";
+		resultMessage.innerHTML = 'Message sent, I will reach you asap!';
+	}, function(error) {
+		resultMessage.classList.add('danger');
+		resultMessage.style.display = "inline";
+		resultMessage.innerHTML = error;
+	});;
 }, false);
