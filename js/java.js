32d19a575c9acc79cd9418fb543d7666e6fbbf39
saberhosneydev <ssber277@gmail.com>
2020-12-19 02:44:08 +0200

    Reverting to v3 until upgrading to v5

---
+++ js/java.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+	var typed = new Typed('#key', {
+		strings: ['Developer', 'Designer', 'Gamer', 'Thinker'],
+		typeSpeed: 50,
+		backSpeed: 50,
+		loop:true
+	});
+	var typed2 = new Typed('#secondKe3y', {
+		strings: ['Code', 'Design', 'Play', 'Debug'],
+		typeSpeed: 50,
+		backSpeed: 50,
+		loop:true
+	});
+});
